5d06e52abc91a3584bd15e8cba4dedf282cedd5a
Erik Assum <erik@iterate.no>
2016-05-22 00:11:49 +0200

    initial commit

---
+++ src/sms_spam/core.clj
@@ -0,0 +1,32 @@
+(ns sms-spam.core
+    (:require
+     [cheshire.core :as json]
+     [clojure.string :as str]
+     [clj-http.client :as http])
+  (:gen-class))
+
+(def template {:deliveryReportGates ["w8Tg0tC5"],
+               :destinationTON "MSISDN",
+               :source "evalg2016",
+               :sourceTON "ALPHANUMERIC",
+               :platformPartnerId "380",
+               :platformId "SMSC",
+               })
+
+(defn payload [template message destination]
+  (merge template {:destination destination
+                   :userData message}))
+
+(defn send-sms [uname passwd payload]
+  (http/post "https://wsx.sp247.net/sms/send" {:form-params payload
+                                               :content-type :json
+                                               :basic-auth [uname passwd]}))
+
+(defn send-all [uname passwd template message destinations]
+  (doseq [destination destinations]
+       (send-sms uname passwd (payload template message destination))))
+
+(defn -main [uname passwd message-file phonenumber-file]
+  (let [destinations (slurp phonenumber-file)
+        message (slurp message-file)]
+    (send-all uname passwd template message (str/split-lines destinations))))
---
+++ test/sms_spam/core_test.clj
@@ -0,0 +1,7 @@
+(ns sms-spam.core-test
+  (:require [clojure.test :refer :all]
+            [sms-spam.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
